04044c5003469944801512483bd6011723fe46e8
김지헌 <youwa65@gmail.com>
2022-10-05 00:54:41 +0900

    트라이캐치문 올리기

--- routes/comments.js
+++ routes/comments.js
@@ -7,60 +7,79 @@ const comments = require('../schemas/comments');
 // 게시물 작성 
 //포스트 아이디를 같이 집어넣어줘서 해당 게시글의 댓글이란걸 알수있음
   router.post('/:_postId', async (req,res)=>{
-    const { _postId } = req.params;                         //입력받은 아이디값이들어옴
-    const {user, password, content} = req.body;             //저장해야할 정보를 받아와서 변수에 등록시킨다. req.body에 정보가 들어있음
-    console.log(content)
-    const createdAt = new Date();                           //날짜  넣어주기
-    if(content == ""){                                    //content 댓글이 빈칸일시 문구 반환 댓글 보내주기
-      res.json({'message': "댓글을 입력해주세요"});
-    }else{                                                 //스키마 comments 아이디로 정보를 저장하고 만들어줌
+  try{
+      const { _postId } = req.params;                         //입력받은 아이디값이들어옴
+      const {user, password, content} = req.body;             //저장해야할 정보를 받아와서 변수에 등록시킨다. req.body에 정보가 들어있음
+      const createdAt = new Date();                           //날짜  넣어주기
+      if(content == ""){                                    //content 댓글이 빈칸일시 문구 반환 댓글 보내주기
+      res.status(400).send({'message': "댓글을 입력해주세요"});
+      }else{                                                 //스키마 comments 아이디로 정보를 저장하고 만들어줌
       await comments.create({ _postId ,user , password, content, createdAt}); //스키마.db에 정보를 만들어준다
-      res.json({'message': "댓글을 생성하였습니다."});    //메세지 생성 제이슨 형식으로 응답해줌
-    }
+      res.status(201).send({'message': "댓글을 생성하였습니다."});    //메세지 생성 제이슨 형식으로 응답해줌
+      }
+    } catch(error){ //catch가 에러를 받는다.
+      console.log(error)
+      res.status(400).send({'message': "댓글 불러오기 error"}) //에러 400 try catch try문 안에 에러가 나면 catch가 잡아줘서 에러문구를 보내준다.(서버가 꺼지지 않음)
+    }   
   })
 
   //해당 게시물 댓글 가져오기 해당 게시물 댓글만 가져온다
   router.get('/:_postId',async (req,res)=>{
- const { _postId } = req.params                                   //아이디 값을 받아온다 //파라미터로 받아온 아이디와 몽고디비에 저장된 아이디 중 일치하는것을 찾아서 가져온다.                                                           
- const commentAll = await comments.find({_postId}).sort({createdAt: "desc" }) //.sort({ createdAt: "desc" }) createdAt기준으로 내림차순
- const comment = commentAll.map((comment)=> {                         ///필요한 요구값만 보여주기 위함
-  return {  
-    commentId : comment._id,
-    user : comment.user,
-    content : comment.content,
-    createdAt :comment.createdAt
-  }})
-    res.json({ data: comment})
+  try{
+      const { _postId } = req.params                                   //아이디 값을 받아온다 //파라미터로 받아온 아이디와 몽고디비에 저장된 아이디 중 일치하는것을 찾아서 가져온다.                                                           
+      const comments = await comments.find({_postId}).sort({createdAt: "desc" }) //.sort({ createdAt: "desc" }) createdAt기준으로 내림차순
+      const comment = comments.map((comment)=> {                     //map은 하나씩 순회하여 값을 복사해줌 [1,2,3,4,5] 배열로 반환된다.
+      return {  
+      commentId : comment._id,
+      user : comment.user,
+      content : comment.content,
+      createdAt :comment.createdAt
+        }})
+    res.status(201).json({ data: comment})
+    } catch(error){ //catch가 에러를 받는다.
+    console.log(error)
+    res.status(400).send({'message': "댓글 불러오기 error"}) //에러 400 try catch try문 안에 에러가 나면 catch가 잡아줘서 에러문구를 보내준다.(서버가 꺼지지 않음)
+    }
   })
 
 //해당 게시물의 댓글 수정해보기
-router.put('/:_commentId', async (req,res)=>{               //일단 아이디 받아오고 일치하는지 확인 해당 
-  const { _commentId } = req.params                         //입력한 코멘트 아이디를 가져옴
-  const {password , content} =req.body                            //입력한 password와 content받아옴
-  if (content == "") {                                                //content가 빈칸이면  메세지 출력
-    res.json({'message': "댓글을 입력해주세요"});
-  }else { 
-    const comment = await comments.findOne({ _id: _commentId }); //아이디와 일치한 값을 코멘트에서 찾아옴 중복되면 한개만 쓰면됨 구조할당 분해 찾을수 있음//
-    await comments.updateOne({ _id : _commentId }, { $set: {password,content } }); //일치하면 _id를 찾아서  패스워드랑 콘텐트수정
-    res.json({data : comment})    
-    res.json({'message': "댓글을 수정했습니다"})     
-}   
+router.put('/:_commentId', async (req,res)=>{    //일단 아이디 받아오고 일치하는지 확인 해당 
+  try{         
+      const { _commentId } = req.params                         //입력한 코멘트 아이디를 가져옴
+      const {password , content} =req.body                            //입력한 password와 content받아옴
+      if (content == "") {                                                //content가 빈칸이면  메세지 출력
+        res.json({'message': "댓글을 입력해주세요"});
+        }else { 
+        const comment = await comments.findOne({ _id: _commentId }); //아이디와 일치한 값을 코멘트에서 찾아옴 중복되면 한개만 쓰면됨 구조할당 분해 찾을수 있음//
+        await comments.updateOne({ _id : _commentId }, { $set: {password,content } }); //일치하면 _id를 찾아서  패스워드랑 콘텐트수정
+        res.status(201).json({data : comment})    
+        res.status(400).send({'message': "댓글을 수정했습니다"})            
+        }
+      } catch(error){ //catch가 에러를 받는다.
+        console.log(error)
+        res.status(400).send({'message': "댓글 불러오기 error"}) //에러 400 try catch try문 안에 에러가 나면 catch가 잡아줘서 에러문구를 보내준다.(서버가 꺼지지 않음)
+      }   
 })
 
 //해당 댓글 삭제
 router.delete('/:_commentId',async (req,res)=>{
-  const { _commentId } = req.params;                              //아이디 정보를 받아온다 delete 누르면 작동함
-  const { password } = req.body;                                 //바디에 내가 적으면 여기에 뜸 정보수정할꺼
-  const commnet = await comments.findOne({ _id : _commentId });   //일치하는 코멘트트 아이디 값을 찾음
-  if ( commnet.password == password){
-    await comments.deleteOne({ _id : _commentId });               //해당하는 아이디를 삭제해라
-  } else {                                                        //한개를 삭제한다.
-    return res.status(400).json({                                 //else문 쓸때 예외처리하기
-    success: false,
-    msg: "비밀번호가 일치하지 않습니다!",
-    });
-    }
-  res.json({"message":"댓글을 삭제하였습니다."}); 
+  try{
+      const { _commentId } = req.params;                              //아이디 정보를 받아온다 delete 누르면 작동함
+      const { password } = req.body;                                 //바디에 내가 적으면 여기에 뜸 정보수정할꺼
+      const commnet = await comments.findOne({ _id : _commentId });   //일치하는 코멘트트 아이디 값을 찾음
+      if ( commnet.password == password){
+      await comments.deleteOne({ _id : _commentId });               //해당하는 아이디를 삭제해라
+      } else {                                                        //한개를 삭제한다.
+      return res.status(400).json({                                 //else문 쓸때 예외처리하기
+      success: false,
+      msg: "비밀번호가 일치하지 않습니다!",
+      });
+      }
+      res.status(201).send({"message":"댓글을 삭제하였습니다."}); 
+    } catch(error){ //catch가 에러를 받는다.
+      console.log(error)
+      res.status(400).send({'message': "댓글 불러오기 error"}) //에러 400 try catch try문 안에 에러가 나면 catch가 잡아줘서 에러문구를 보내준다.(서버가 꺼지지 않음)
+    }   
 })
 
 
--- routes/posts.js
+++ routes/posts.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-const Post = require('../schemas/posts');
+const Post = require('../schemas/posts'); //폴더 밖에 나가서 경로를 찾아서 ../넣음
 
-
+// try{실행할코드}catch(error){ //catch가 에러를 받는다.
+//   console.log(error)
+//   res.status(400).send({'message': "작성실패error"}) //에러 400 try catch try문 안에 에러가 나면 catch가 잡아줘서 에러문구를 보내준다.(서버가 꺼지지 않음)
+//  }  
 
 
 ///몽구스의 스키마를 이용하여몽고 디비에 저장 하기
 router.post('/', async (req, res) => {   //post누르면 정보가 담겨있음
-    const { user, password, title, content, } = req.body; //저장해야할 정보를 받아와서 변수에 등록시킨다. req.body에 정보가 들어있음
+  try{
+    const {uesr, password, title, content, } = req.body; //저장해야할 정보를 받아와서 변수에 등록시킨다. req.body에 정보가 들어있음
     const createdAt = new Date(); //날짜 지정 yyyddmmm이거 쓰기 나중에 하자!!
-    await Post.create({ user, password, title, content,createdAt}); //스키마.db에 정보를 만들어준다
-   
-    res.send({'message': "게시글을 생성하였습니다."});  //스키마 Post 아이디로 정보를 저장하고 만들어줌
+    await Post.create({ uesr, password, title, content,createdAt}); //스키마.db에 정보를 만들어준다  //앞에는 키값 : 벨류값
+    res.status(201).send({'message': "게시글을 생성하였습니다."});  //스키마 Post 아이디로 정보를 저장하고 만들어줌
+   }catch(error){ //catch가 에러를 받는다.
+    console.log(error)
+    res.status(400).send({'message': "작성실패error"}) //에러 400 try catch try문 안에 에러가 나면 catch가 잡아줘서 에러문구를 보내준다.(서버가 꺼지지 않음)
+   }   
 });
 
 //몽고 디비에 받아오기
@@ -24,32 +31,41 @@ router.post('/', async (req, res) => {   //post누르면 정보가 담겨있음
 
 //게시글 불러오기 내림차순
 router.get('/',async (req,res)=>{// get으로 데이터를 불러올꺼임
-  const posts = await Post.find().sort({ createdAt: "desc" }) ////내림차순 차을때 사용한다!!!!
-  const post = posts.map((posts)=> {
-      return {  ///필요값만 보여주기 위함
-        postId : posts._id,
-        user : posts.user,
-        title : posts.title,
-        createdAt :posts.createdAt
+  try{
+  const posts = await Post.find().sort({ createdAt: "desc" }) ////내림차순 차을때 사용한다!!!! //.sort("-createdAt");
+  const post = posts.map((post)=> {   //배열 순회 하면서 복제 시킨다.(배열에서만 쓸수있다.) .map((순회하는 배열하는 아이템)=>{바꿔주는 함수(비지니스 로직)})
+      return {  ///필요값만 보여주기 위함   //filter() ,find()  숙제 같이쓴다.
+        postId : post._id,
+        user : post.user,
+        title : post.title,
+        createdAt :post.createdAt
       }
     })
-    res.json({data : post});//찾아온 정보를 data에 넣어서 보내준다    
+    res.status(201).json({data : post});//찾아온 정보를 data에 넣어서 보내준다 (우리가 보내면 기본적으로 data에 받는다. data굳이 안적어두됨)  
+      } catch(error){ //catch가 에러를 받는다.
+      console.log(error)
+      res.status(400).send({'message': "게시글 목록조회 error"}) 
+     }    
+
   })
 
   //상세 페이지 조회
-  router.get('/:_postId' ,async (req,res)=>{
+router.get('/:_postId' ,async (req,res)=>{
+  try{
     const { _postId } = req.params; //썬더에 입력하면 정보 받아옴
-    const postOne = await Post.find({_id :_postId}); //Post.findOne({_id :_postId}), 아이디가 일치하는것을 찾아옴
-    const post = postOne.map((posts)=> {
-      return {                              ///필요값만 보여주기 위함
-        postId : posts._id,
-        user : posts.user,
-        title : posts.title,
-        content : posts.content,
-        createdAt :posts.createdAt
+    const postOne = await Post.findOne({_id :_postId}); //Post.findOne({_id :_postId}), 아이디가 일치하는것을 찾아옴
+    const post = {       //map함수는 배열이 여러개 일때 사용하면 좋다. 1개일때는 불필요한 코드
+        postId : postOne._id,   //재할당해서 변수에 넣어준다. postOne의 값을 ._id등등 필요한 값만 추출해서() 키값 : 추출한 값으로 바꿔줌)
+        user : postOne.user,
+        title : postOne.title,
+        content : postOne.content,
+        createdAt :postOne.createdAt
         }
-       })
-        res.json({ data: post})              
+        res.status(201).json({ data: post})
+      }catch(error){ 
+        console.log(error)
+        res.status(400).send({'message': "상세 게시글 목록조회 error"}) 
+       }                   
   })
 
 
@@ -58,6 +74,7 @@ router.get('/',async (req,res)=>{// get으로 데이터를 불러올꺼임
 
 // 게시글 수정하기 하기
 router.put("/:_postId", async (req, res) => {
+  try{
   const { _postId } = req.params;                 //아이디 정보를 받아옴 내가 put누르면 정보가 담겨있음
   const { password, title, content} = req.body;   //바디에 내가 적으면 여기에 뜸 헷갈리면 찍어보자
   const posts = await Post.findOne({ _id: _postId }); //중복되면 한개만 쓰면됨 구조할당 분해 찾을수 있음//
@@ -68,19 +85,22 @@ router.put("/:_postId", async (req, res) => {
       success: false,
       msg: "비밀번호가 일치하지 않습니다!",
       });
-      }
-  res.json({data : posts})
-  res.send({'message': "게시글을 수정하였습니다."})
+    }
+  res.status(201).send({'message': "게시글을 수정하였습니다."})
+    }catch(error){ 
+    console.log(error)
+    res.status(400).send({'message': "작성실패error"}) 
+    }               
 })
 
 
 
 //게시물 지우기 비밀번호 적어서 해야함
 router.delete("/:_postId", async (req, res) => {
+  try{
   const { _postId } = req.params;                        //아이디 정보를 받아온다 delete 누르면 작동함
   const { password } = req.body;                        //바디에 내가 적으면 여기에 뜸 정보수정할꺼
   const posts = await Post.findOne({ _id : _postId }); //일치하는 포스트 아이디 값은
-  console.log(posts.password ,password)
   if (posts.password == password) {                     //pw가 일치하면 통과함
   await Post.deleteOne({ _id : _postId });              //해당하는 아이디를 삭제해라
   }else {
@@ -89,7 +109,11 @@ router.delete("/:_postId", async (req, res) => {
     msg: "비밀번호가 일치하지 않습니다!",
     });
     }
-  res.json({"message":"게시글을 삭제하였습니다."}); 
+  res.status(201).send({"message":"게시글을 삭제하였습니다."}); 
+  }catch(error){ 
+  console.log(error)
+  res.status(400).send({'message': "게시글 삭제 error"}) 
+  }          
 });
 
 
